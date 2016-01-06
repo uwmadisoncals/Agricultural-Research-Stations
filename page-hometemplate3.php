cd1ca514521d098d80e104bba69983d26018e634
Al Nemec <al.nemec@gmail.com>
2016-01-06 10:16:24 -0600

    Added "more news" button on to homepage.

--- page-hometemplate3.php
+++ page-hometemplate3.php
@@ -45,6 +45,9 @@ get_header(); ?>
 						<?php
 						echo do_shortcode('[wp_rss_retriever url="http://news.cals.wisc.edu/feed/" orderby="date" items="2" excerpt="15" read_more="true" source="false" new_window="true" thumbnail="false" cache="7200"]'); 
 						?>
+						
+						<a href="http://news.cals.wisc.edu" target="_blank" class="moreButton"><svg height="512px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" width="512px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><polygon points="160,128.4 192.3,96 352,256 352,256 352,256 192.3,416 160,383.6 287.3,256 "/></svg></a>
+						
 						</div>
 					
 					</div><!-- END .box.videos -->
